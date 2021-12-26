0bb12e1685adc9c374d75af9a0a07827d62ef3d4
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-12-26 15:06:30 -0800

    final commit1

--- distanceCalculator/main.cpp
+++ distanceCalculator/main.cpp
@@ -1,14 +1,32 @@
-//
-//  main.cpp
-//  distanceCalculator
-//
-//  Created by Abel Christy Anand on 12/25/21.
-//
-
+// Abel Anand
 #include <iostream>
+#include <cmath>
+using namespace std;
+    
+float distanceCalcuator(int x1, int y1, int x2, int y2) {
+    float f1 = pow(y2-y1, 2);
+    float f2 = pow(x2-x1, 2);
+    
+    float distance = sqrt(f1+f2);
+    
+    return distance;
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+struct points {
+    float x;
+    float y;
+};
+
+int main() {
+    points points1;
+    points points2;
+    cout << "Enter the first x value\n";
+    cin >> points1.x;
+    cout << "Enter the first y value\n";
+    cin >> points1.y;
+    cout << "Enter the second x value\n";
+    cin >> points2.x;
+    cout << "Enter the second y value\n";
+    cin >> points2.y;
+    cout << distanceCalcuator(points1.x, points1.y, points2.x, points2.y);
 }
